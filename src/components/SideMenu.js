331dfd4bca4941a5cecd0f86a68d442e9d720713
Muhammad Khalid <109454498+DigitalKhalid@users.noreply.github.com>
2022-11-08 10:23:48 +0500

    raw-2

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -2,45 +2,50 @@ import React, { useState } from 'react'
 import './SideMenu.css'
 
 export const SideMenu = () => {
-    const [isOpen, setIsOpen] = useState(false)
+    const buttonHeight = 50
 
-    // const style = {
-    //     overflow: 'hidden',
-    //     height: 0,
-    //     transition: '2s ease'
-    // }
+    const subMenuStyle = {
+        overflow: 'hidden',
+        height: 0,
+    }
 
-    const toggleSubMenu = (id) => {
-        let item = document.getElementById(id)
-        console.log(item.style.height)
-        if (item.style.height === '0px') {
-            document.getElementById(id).style.overflow = 'hidden'
-            document.getElementById(id).style.height = '100px'
+    const toggleSubMenu = (id, items, event) => {
+        let menu = document.getElementById(id)
+        if (menu.style.height === '0px') {
+            document.getElementById(id).style.height = `${buttonHeight*items}px`
             document.getElementById(id).style.transition = '0.2s'
+            document.getElementById(`${id}-dropdown`).style.transform ='rotate(90deg)'
+            
         } else {
-            document.getElementById(id).style.overflow = 'hidden'
             document.getElementById(id).style.height = 0
             document.getElementById(id).style.transition = '0.2s'
+            document.getElementById(`${id}-dropdown`).style.transform ='rotate(0deg)'
         }
     }
 
     return (
         <div>
             <div className='side-menu'>
+                <div className="close-btn">
+                    <i className='fa fa-times'></i>
+                </div>
+
                 <div className="menu">
                     <div className="item"><button><i className='fa fa-desktop'></i>Dashboard</button></div>
 
                     <div className="item">
-                        <button className='sub-btn' onClick={() => toggleSubMenu('customers')}><i className='fa fa-users'></i>Customers <i className='fa fa-angle-right dropdown'></i></button>
-                        <div className="sub-menu" id='customers'>
+                        <button className='sub-btn' onClick={() => toggleSubMenu('customers', 2)}><i className='fa fa-users'></i>Customers <i className='fa fa-angle-right dropdown' id='customers-dropdown'></i></button>
+
+                        <div className="sub-menu" style={subMenuStyle} id='customers'>
                             <button className='sub-item'><i className='fa fa-user-plus'></i>Add New</button>
                             <button className='sub-item'><i className='fa fa-list'></i>View List</button>
                         </div>
                     </div>
 
                     <div className="item">
-                        <button className='sub-btn' onClick={() => toggleSubMenu('subdealers')}><i className='fa fa-user'></i>Sub Dealers<i className='fa fa-angle-right dropdown'></i></button>
-                        <div className="sub-menu" id='subdealers'>
+                        <button className='sub-btn' onClick={() => toggleSubMenu('subdealers', 2)}><i className='fa fa-user'></i>Sub Dealers<i className='fa fa-angle-right dropdown' id='subdealers-dropdown'></i></button>
+
+                        <div className="sub-menu" style={subMenuStyle} id='subdealers'>
                             <button className='sub-item'><i className='fa-solid fa-person-circle-plus'></i>Add New</button>
                             <button className='sub-item'><i className='fa fa-list'></i>View List</button>
                         </div>
